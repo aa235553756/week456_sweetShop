9bbf2e37c1750383d5990e29bd979de0a05457f3
ChiaYang <aa235553756@gmail.com>
2022-11-08 14:06:30 +0800

    Update 2022-11-08T06:06:23.943Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,23 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  var invoice01 = $('.checkout__invoiceChoose')[0];
+  var invoice02 = $('.checkout__invoiceChoose')[1];
+  var option01 = $('.checkout__invoiceOption')[0];
+  var option02 = $('.checkout__invoiceOption')[1];
+  $(option01).removeClass('d-none');
+  $(invoice01).addClass('active');
+  $(invoice01).on('click', function () {
+    $(invoice02).removeClass('active');
+    $(invoice01).addClass('active');
+    $(option01).removeClass('d-none');
+    $(option02).addClass('d-none');
+  });
+  $(invoice02).on('click', function () {
+    $(invoice01).removeClass('active');
+    $(invoice02).addClass('active');
+    $(option02).removeClass('d-none');
+    $(option01).addClass('d-none');
+  });
 });
 //# sourceMappingURL=all.js.map
